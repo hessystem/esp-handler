b92a1009557eb2db6e870000f2af7b68276f7dcf
anvornial <voratanra.ab@gmail.com>
2015-03-25 13:42:45 +0700

    andra : aku modifikasi struktur kelas, nambahin metoda untuk konek tcp dsb#

--- esphandler.h
+++ esphandler.h
@@ -3,6 +3,7 @@
 //there are other method which i haven't added yet such as check join devices from certain IP, TCP/IP connection status
 //Send TCP/IP data and so on
 //parameters from these method are still not correct
+
 #include "Arduino.h";
 #include "SoftwareSerial.h"
 class esphandler
@@ -12,13 +13,14 @@ public:
 	esphandler(int RX,int TX);
 	~esphandler(int RX,int TX);
 	void checkCommand(String command);
+	String sendATCommand(String command, String response);
 	String getResponse(String resp);
-	String joinAP(String ssid,String password);
 	String firmwareVersion();
 	String listAP();
+	String joinAP(String ssid, String password);
 	String QuitAP();
 	String getIP();
-	String setparamAP();
-	String wifiMode(char mode);
-	String TCPUDP(char p);
+	String setparamAP(); // i dont know what is this ???
+	String wifiMode(int mode);
+	String startClientTCP(String chan, String host, String port);	
 };
