ee79554e2f4894038a4ceda02bcc0afbeab66866
hessystem <hessystem.studio@gmail.com>
2015-03-27 18:08:43 +0700

    Update esphandler.h

--- esphandler.h
+++ esphandler.h
@@ -3,7 +3,8 @@
 //there are other method which i haven't added yet such as check join devices from certain IP, TCP/IP connection status
 //Send TCP/IP data and so on
 //parameters from these method are still not correct
-
+#ifndef esphandler_h
+#define esphandler_h
 #include "Arduino.h";
 #include "SoftwareSerial.h"
 #define this->p this.p
@@ -25,3 +26,4 @@ public:
 	String wifiMode(int mode);
 	String startClientTCP(String chan, String host, String port);	
 };
+#endif
