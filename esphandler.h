987932ed08f3a9a263463238d0ec162cb1b838ff
hessystem <hessystem.studio@gmail.com>
2015-03-27 22:01:11 +0700

    Update esphandler.h
    wifimodule attribute isn't quite right

--- esphandler.h
+++ esphandler.h
@@ -1,8 +1,11 @@
-//March,24
+//March,24 th
 //I added some simple method
 //there are other method which i haven't added yet such as check join devices from certain IP, TCP/IP connection status
 //Send TCP/IP data and so on
 //parameters from these method are still not correct
+//March 27th
+//some abstract function finally implemented
+//setparam ap is not completed yet
 #ifndef esphandler_h
 #define esphandler_h
 #include "Arduino.h";
@@ -11,7 +14,7 @@
 class esphandler
 {
 public:
-	SoftwareSerial wifimodul(0,0);
+	SoftwareSerial wifimodule(0,0);
 	esphandler(int RX,int TX);
 	~esphandler(int RX,int TX);
 	void checkCommand(String command);
@@ -26,4 +29,3 @@ public:
 	String wifiMode(int mode);
 	String startClientTCP(String chan, String host, String port);	
 };
-#endif
