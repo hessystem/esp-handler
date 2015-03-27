bb934da2940ecf0359f3a8d7a65c0b0550a47dde
hessystem <hessystem.studio@gmail.com>
2015-03-27 18:05:37 +0700

    Update esphandler.cpp
    fixing woring command

--- esphandler.cpp
+++ esphandler.cpp
@@ -1,27 +1,43 @@
 #include "esphandler.h";
 #include "Arduino.h";
+
 esphandler::esphandler(int RX,int TX)
 {
-	this.wifimodul(RX,TX);
+	this.wifimodule(RX,TX);
 }
 void esphandler::Checkcommand(String command);
 {
 	command=String.toLowerCase(command);
-	if(command.equals(String.toLowerCase("working")))
+	if(command.equals("working"))
 	{
-		this.getResponse("it's working");
+		Serial.println(this.getResponse("AT"));
 	}
-	else if(command.equals(String.toLowerCase("restart")))
+	else if(command.equals("restart"))
 	{
-		this.getResponse("Restart done");
+		//this.getResponse("Restart done");
 	}
-	else if(command.equals(String.toLowerCase("firmware")))
+	else if(command.equals("firmware"))
 	{
-		this.getResponse(this.firmwareVersion());
+		//this.getResponse(this.firmwareVersion());
+	}
+	else if(command.equals("list_ap"))
+	{
+		this.getResponse
 	}
 	else
 	{
-		
+	
 	}
+}
+String esphandler::getResponse(String resp)
+{
+	this.wifimodule.print(resp);
+	String response="";
+	while(this.wifimodule.available())
+	{
+		char l = this.wifimodule.read();
+		response = response+l;
+	}
+	return response;
 }
 
